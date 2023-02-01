ae8e3a3ccca7d97162e1e65e8270f8c1058097c9
Carmelocrlpz <ccr376@gmail.com>
2023-01-31 21:57:20 -0600

    add video robotic

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+
+export class HomeComponent implements OnInit {
+public video = "";
+
+constructor() { }
+
+ngOnInit(){
+  this.video = "assets/video/robotic2.mp4"
+}
 
 }
