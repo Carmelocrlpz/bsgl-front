f845b1f84a5dabe048c0388b61bcdc1b96fd5f00
MiguelEmmanuel46 <sombrass46@gmail.com>
2023-02-03 11:34:45 -0600

    consultoria add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,21 @@ import { TableModule } from 'primeng/table';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { EspecialidadesComponent } from './especialidades/especialidades.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { ConsultoriaComponent } from './consultoria/consultoria.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    EspecialidadesComponent,
+    ServiciosComponent,
+    ConsultoriaComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
