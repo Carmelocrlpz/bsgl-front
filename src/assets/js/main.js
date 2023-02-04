5921e4be286b7a542ac4d1f6cf973b2422168737
MiguelEmmanuel46 <sombrass46@gmail.com>
2023-02-04 02:21:35 -0600

    services and specialities changes

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -67,3 +67,61 @@ menuLinks.foreach(menuLink => {
 });
 */
 
+/*
+$(function(){
+
+    var texto = "NUESTRO OBJETIVO ES BRINDAR LA TECNOLOGÍA PARA AUTOMATIZAR SUS PROCESOS";
+    maquina("slogan",texto,100);
+
+});
+
+
+function maquina(contenedor,texto,intervalo){
+   // Calculamos la longitud del texto
+   longitud = texto.length;
+   // Obtenemos referencia del div donde se va a alojar el texto.
+   tagHtml = document.getElementById(contenedor);
+   tagHtml.innerHTML="";
+   var i=0;
+   // Creamos el timer
+   timer = setInterval(function(){
+      // Vamos añadiendo letra por letra y la _ al final.
+      tagHtml.innerHTML = tagHtml.innerHTML.substr(0,tagHtml.innerHTML.length-1) + texto.charAt(i) + "_";
+      // Si hemos llegado al final del texto..
+      if(i >= longitud){
+         // Salimos del Timer y quitamos la barra baja (_)
+         clearInterval(timer);
+         tagHtml.innerHTML = tagHtml.innerHTML.substr(0,longitud);
+
+         maquina("slogan",texto,100);
+         return true;
+      } else {
+         // En caso contrario.. seguimos
+         i++;
+      }
+   },intervalo);
+};*/
+
+
+function escribirEnPantalla(texto){
+  let arr=texto.split("");
+  i=0;
+ let intervalo= setInterval(function(){
+    if(i==arr.length-1){ 
+      document.getElementById("slogan").innerHTML+=arr[i];
+      clearInterval(intervalo);} 
+    else {
+    if(arr[i]==" "){
+     document.getElementById("slogan").innerHTML+=arr[i];
+     document.getElementById("slogan").innerHTML+=arr[i+1];
+      i+=2;
+    }else{
+    document.getElementById("slogan").innerHTML+=arr[i];
+    i++;
+      }
+    }
+  },100); 
+}
+
+escribirEnPantalla("NUESTRO OBJETIVO ES BRINDAR LA TECNOLOGÍA PARA AUTOMATIZAR SUS PROCESOS");
+
