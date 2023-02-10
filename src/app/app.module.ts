eb8268907e52057a12c42126ada8ded47d1af561
Carmelocrlpz <ccr376@gmail.com>
2023-02-09 22:13:54 -0600

    update contact section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -25,7 +26,8 @@ import { ContactoComponent } from './contacto/contacto.component';
   ],
   imports: [
     BrowserModule,
-    TableModule
+    TableModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/contacto/contacto.component.ts
+++ src/app/contacto/contacto.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contacto',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./contacto.component.css']
 })
 export class ContactoComponent {
+
+  contactForm: FormGroup;
+
+    constructor(private formBuilder: FormBuilder) {
+      this.contactForm = this.formBuilder.group({
+        name: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        telefono: ['', Validators.required],
+        message: ['', Validators.required]
+      });
+    }
+
+    onSubmit() {
+    // implement the form submission logic here
+  }
 
 }
