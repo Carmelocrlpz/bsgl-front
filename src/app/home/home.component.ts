2bdb6fab98383a1a912b1190cc73b4ea7797a4f0
Carmelocrlpz <ccr376@gmail.com>
2023-02-13 21:50:46 -0600

    update text

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, Renderer2, ViewChild  } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,60 +7,56 @@ import { Component, ElementRef, OnInit, Renderer2, ViewChild  } from '@angular/c
 })
 
 export class HomeComponent implements OnInit {
-  public writing!:any;
-  public myTimeout!:any;
+  public writing!: any;
+  public myTimeout!: any;
 
   @ViewChild('slogan') slogan!: ElementRef;
-  
-  constructor( private renderer2: Renderer2){ }
 
-  ngOnInit(){}
+  constructor(private renderer2: Renderer2) { }
 
-  ngAfterViewInit() 
-  {
+  ngOnInit() { }
+
+  ngAfterViewInit() {
     this.escribirPantalla("NUESTRO OBJETIVO ES BRINDAR LA TECNOLOGÍA PARA AUTOMATIZAR SUS PROCESOS");
     setInterval(() => this.validar(), 8000);
   }
 
-  escribirPantalla(slo:string)
-  {    
+  escribirPantalla(slo: string) {
     let arraySlo = slo.split('');
-    let i=0;
+    let i = 0;
     let elemntSlogan = this.slogan.nativeElement;
-    let printStr = setInterval(function(){
-      //this.renderer2.setProperty(this.slogan, 'innerHTML', arraySlo[i]); 
+    let printStr = setInterval(function() {
+      //this.renderer2.setProperty(this.slogan, 'innerHTML', arraySlo[i]);
       elemntSlogan.innerHTML += arraySlo[i];
-              i++;
-      if (i === arraySlo.length){
+      i++;
+      if (i === arraySlo.length) {
         clearInterval(printStr);
-        i=0;
+        i = 0;
       }
-    },100);  
+    }, 100);
   }
 
-/********************BUCLE slogan****************************************/
+  /********************BUCLE slogan****************************************/
   myStopFunction() { clearTimeout(this.myTimeout); }
 
-  validar()
-  { 
+  validar() {
     let mielemento = this.slogan.nativeElement.innerHTML;
-      if(!mielemento){ 
-        this.escribirPantalla("NUESTRO OBJETIVO ES BRINDAR LA TECNOLOGÍA PARA AUTOMATIZAR SUS PROCESOS");
-        this.myStopFunction(); 
-        this.myTimeout = setTimeout(() => this.repeticion(), 7800);
-      }else{
-           this.slogan.nativeElement.innerHTML = "";
-      } 
+    if (!mielemento) {
+      this.escribirPantalla("NUESTRO OBJETIVO ES BRINDAR LA TECNOLOGÍA PARA AUTOMATIZAR SUS PROCESOS");
+      this.myStopFunction();
+      this.myTimeout = setTimeout(() => this.repeticion(), 7800);
+    } else {
+      this.slogan.nativeElement.innerHTML = "";
+    }
   }
-   
-  repeticion() 
-  { 
+
+  repeticion() {
     setTimeout(() => this.validar(), 200);
   }
-/************************************************************/
+  /************************************************************/
 
-  
 
-  
+
+
 
 }
