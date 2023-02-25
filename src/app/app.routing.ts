e4122efc96c1927aa0d6dcbcd1e0406d4379fb98
MiguelEmmanuel46 <sombrass46@gmail.com>
2023-02-25 00:24:20 -0600

    resetpass works on production

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,35 +1,41 @@
-import { ModuleWithProviders } from '@angular/core';
+import { ModuleWithProviders, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'
 
 
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-//import { AppComponent } from './app.component';
-//import { HomeComponent } from './home/home.component';
+
 import { HeaderComponent } from './header/header.component';
-/*import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './home/home.component';
 import { NosotrosComponent } from './nosotros/nosotros.component';
 import { EspecialidadesComponent } from './especialidades/especialidades.component';
 import { ServiciosComponent } from './servicios/servicios.component';
 import { ConsultoriaComponent } from './consultoria/consultoria.component';
 import { ContactoComponent } from './contacto/contacto.component';
-import { FooterComponent } from './footer/footer.component';*/
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 import { PanelComponent } from './components/panel/panel.component';
 import { PasswordResetComponent } from './components/password-reset/password-reset.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { ErrorComponent } from './components/error/error.component';
 
+
 //definir rutas
 const appRoutes: Routes = [
-	{ path: '', component: HeaderComponent},
+	{ path: '', component: HomeComponent},
+	{ path: '#nosotros', component: NosotrosComponent },
+	{ path: '#especialidades', component: EspecialidadesComponent },
+	{ path: '#servicios', component: ServiciosComponent },
+	{ path: '#consultoria', component: ConsultoriaComponent },
+	{ path: '#contacto', component: ContactoComponent },
 	{ path: 'login', component: LoginComponent },
 	{ path: 'logout/:sure', component: LoginComponent },
 	{ path: 'password-reset', component: PasswordResetComponent },
 	{ path: 'forgot-password', component: ForgotPasswordComponent },
 	{ path: 'register', component: RegisterComponent },
-	{ path: 'panel', component: PanelComponent },
+	{ path: 'panel', component: PanelComponent },	
 	{ path: '**', component: ErrorComponent }
 ];
+
 //exportarr configuracion
 export const appRoutingProviders: any[] = [];
-export const routing: ModuleWithProviders<any> = RouterModule.forRoot(appRoutes,{ useHash: true });
+export const routing: ModuleWithProviders<any> = RouterModule.forRoot(appRoutes);
